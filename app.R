37d3ae883e1f2f88a304b530170b8d513b0c4e3b
Simona Bisiani <52672651+simonabisiani@users.noreply.github.com>
2023-03-16 23:19:25 +0000

    shiny_app

--- app.R
+++ app.R
@@ -5,201 +5,72 @@ library(shinythemes)
 library(htmltools)
 library(RColorBrewer)
 
-# THERE ARE A FEW DATASETS:
+# Load your dataset
+observations <- read_csv("directory1.csv")
+titles_in_lads <- read_csv("titles_in_lad_list.csv")
+counts_for_lads <- read_csv("counts_for_lads.csv")
+urls <- read_csv("urls2.csv")
 
-# -LADS by ONS: purely geographic 
-geo_LAD <- read_csv("Local_Authority_Districts_(December_2022)_Boundaries_UK_BFE.csv")
-
-# -titles_by_lad: count of titles within a LAD
-lad_titles <- read_delim("titles_by_lad.csv", 
-                    delim = ",", escape_double = FALSE,  
-                    locale = locale(), trim_ws = TRUE) %>% 
-  rename("Local Authority District" = LAD,
-         "Number of titles" = n) 
-
-# -directory_with_hyperlocals: directory that needs to be cleaned, purely needed to extract info like ownership, and frequency from
-titles_info <- readRDS("directory_with_hyperlocals.RDS")
-
-# - directory_geo_final, where each title is matched to a LAD
-directory <- readRDS("directory_geo_final.rds")
-
-# - twitter info
-twitter <- readRDS("twitter_handles.RDS")
-
-
-##### STEP ONE: increment amount of information available for each outlet
-directory1 <- directory %>% 
-  left_join(titles_info, by = "Publication") %>% 
-  select(Publication, publisher_recoded, Postcode, LAD, id, Website, Cost, Launch, `Date of closure/launch/change`, Frequency) %>% 
-  left_join(twitter, by = "Publication") %>% 
-  group_by(Publication) %>% 
-  slice_head(n = 1) %>% 
-  mutate(Launch = if_else(is.na(`Date of closure/launch/change`), Launch, `Date of closure/launch/change`)) %>% 
-  select(-`Date of closure/launch/change`) %>% 
-  rename("Owner" = publisher_recoded,
-         "Twitter handle" = twitter_handle) %>% 
+data <- counts_for_lads %>% 
   ungroup() %>% 
-  group_by(LAD) %>% 
-  mutate("Total number of titles in the same Local Authority District" = n())
+  mutate(`Number of titles` =
+           replace_na(`Total number of titles in the same Local Authority District`, 0)) %>% 
+  select(LAD, `Number of titles`) %>% 
+  left_join(titles_in_lads, by = "LAD") %>% 
+  mutate(across(.cols = everything(), ~replace_na(., ""))) %>% 
+  arrange(LAD) %>% 
+  rename("Local Authority District" = LAD)
 
-map_data <- geo_LAD %>% 
-  left_join(lad_titles, by = c("LAD22NM" = "Local Authority District")) %>% 
-  rename(Latitude = "LAT", 
-         Longitude = "LONG") %>% 
-  mutate(`Number of titles` = replace_na(`Number of titles`, 0)) 
+data2 <- observations %>% 
+  select(Publication, Owner, LAD, indep, Frequency, Cost, Website, `Twitter handle`) %>% 
+  group_by(Publication) %>% 
+  summarise(
+    across(everything(),                                   # apply to all columns
+           ~paste0(unique(na.omit(.x)), collapse = "; "))) %>% 
+  mutate(Website = str_remove(Website, "; .*")) %>% 
+  rename("Type of owner" = indep,
+         "Local Authority District" = LAD)
 
+# # polygons
+# library(geojsonio) # struggling to install geojsonio on Linux
+# map <- geojson_read("Local_Authority_Districts_(December_2022)_Boundaries_UK_BUC.geojson", what = "sp")
+# merged_data <- merge(map, map_data, by = "LAD22NM")
+# merged_geojson <- geojson_json(merged_data, lat = "Latitude", lon = "Longitude")
+# geojson_write(merged_geojson, file = "merged_shapefile.shp")
+# map_data <- geojson_read("merged_shapefile.shp", what = "sp")
 
-# Choices for drop-downs
-vars <- c(
-    "Number.of.titles" = "Number.of.titles")
+############################### UI
 
+ui <- navbarPage("Local print and digital news providers in the United Kingdom in March 2023", 
+                 id="nav", theme = shinytheme("paper"),
 
-# polygons
-library(geojsonio) # struggling to install geojsonio on Linux
-map <- geojson_read("Local_Authority_Districts_(December_2022)_Boundaries_UK_BUC.geojson", what = "sp")
-merged_data <- merge(map, map_data, by = "LAD22NM")
-merged_geojson <- geojson_json(merged_data, lat = "Latitude", lon = "Longitude")
-geojson_write(merged_geojson, file = "merged_shapefile.shp")
-
-map_data <- geojson_read("merged_shapefile.shp", what = "sp")
-
-############################### UI  ###############################################################
-
-# here is where the user interface architecture is built
-      
-      # the navbar
-ui <- navbarPage("Local print and digital news providers in the United Kingdom in March 2023", id="nav", theme = shinytheme("paper"),
-           # the interactive map
-           tabPanel("Interactive map",
-                    div(class="outer",
-                        leafletOutput("mymap", height = 600),
-                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                                     draggable = FALSE, top = 150, left = "auto", right = 20, bottom = "auto",
-                                     width = 330, height = "auto",
-                                     h3("Customise your map"),
-                                     selectInput("color", "  Color", vars),
-                        ),
-                        tags$div(id="cite",
-                                 'Please note that the map only provides information relating to specific local authority districts.
-                                 To see information regarding existing titles, head over to the Data Explorer. Data compiled by Simona Bisiani (2023).'
-                        )
-                    )
+           # the data table explorer
+           tabPanel(
+               "Database of outlets", div(class ="outer"),
+               DT::dataTableOutput("ex1")
            ),
            
-           # # the data table explorer
-           # tabPanel(
-           #     "Data Explorer", div(class ="outer"),
-           #     
-           #     # with the different filter options
-           #     fluidRow(
-           #       column(3,
-           #              selectInput("Number of titles",
-           #                          "Number of titles:",
-           #                          c("All",
-           #                            unique(as.character(map_data$`Number of titles`))))
-           #       ),
-           #     ),
-           #     # Create a new row for the table.
-           #     DT::dataTableOutput("ex1")
-           # ),
+           tabPanel(
+             "Database of districts", div(class ="outer"),
+             DT::dataTableOutput("ex2")
+           ),
            
 )
 
-
-############################ SERVER #######################################################
+############################ SERVER
 
 server <- function(input, output, session) {
-    
-pal <- colorFactor("Accent", map_data$`Number.of.titles`)
-  
-     output$mymap <- renderLeaflet({
-        map_data %>% 
-        leaflet() %>%
-        addProviderTiles(providers$CartoDB.Positron) %>% 
-         addPolygons(
-           fillColor = ~pal(Number.of.titles),
-           weight = 0.2,
-           opacity = 1,
-           color = "grey",
-           dashArray = "0",
-           fillOpacity = 0.7) %>% 
-            # addCircleMarkers(~Longitude, ~Latitude, layerId=~LAD22NM,
-            #                                   stroke= FALSE, opacity= 0.7, color=pal, radius = 6.5,
-            #                                   fillColor = pal,
-            #                                   fillOpacity = 0.7) %>% 
-        setView(lng = -3.2765753, lat = 54.7023545, zoom = 6) %>% 
-        semMaxBounds(bounds) 
-     })
-
-    # Modify marker colours depending on user input
-    observe({
-      colorBy <- input$color
-      if (colorBy == "Number.of.titles") {
-        colorData <- map_data[[colorBy]]
-        pal <- colorFactor("Set2", colorData)
-      } 
-      
-      
-     
-      leafletProxy("mymap", data = map_data) %>%
-        
-        clearShapes() %>%
-        
-        addPolygons(
-          fillColor = ~pal(Number.of.titles),
-          weight = 0.2,
-          opacity = 1,
-          color = "grey",
-          dashArray = "0",
-          fillOpacity = 0.7) %>% 
-        
-        # addCircleMarkers(~Longitude, ~Latitude, layerId=~LAD22NM,
-        #                  stroke= FALSE, opacity= 0.7, color=pal(colorData), radius = 6.5,
-        #                  fillColor = pal(colorData),
-        #                  fillOpacity = 0.7,
-        #                  #label = ~sprintf("<strong>%s</strong><br/>%s<br/>Start: %s<br/>End: %s</br>Type: %s</br>Mode: %s</br>Cost: %s</br>City: %s</br> ", Event, Organiser, Start, End, Type, Mode, Cost, City) %>% lapply(htmltools::HTML),
-        #                  labelOptions = labelOptions(
-        #                    style = list("font-weight" = "normal", padding = "3px 8px"),
-        #                    textsize = "11px", direction = "auto")) %>% 
-        
-        addLegend("bottomleft", pal=pal, values=colorData, title=colorBy, opacity = 1,
-                  layerId="colorLegend")
-    })
+        output$ex1 <- DT::renderDataTable(DT::datatable({
+        data
+    }, escape = FALSE, rownames = FALSE))
     
     
-
-# # here is where the filter options for the table appear    
-# 
-#     output$ex1 <- DT::renderDataTable(DT::datatable({
-#         j_table <- j_table %>% 
-#           mutate(URL = paste0("<a href='", URL,"' target='_blank'>", URL,"</a>"))
-#         
-#         
-#         if (input$Topic != "All") {
-#           j_table <- j_table[j_table$Topic == input$Topic,]
-#         }
-#         if (input$Organiser != "All") {
-#             j_table <- j_table[j_table$Organiser == input$Organiser,]
-#         }
-#         if (input$Mode != "All") {
-#             j_table <- j_table[j_table$Mode == input$Mode,]
-#         }
-#         if (input$Type != "All") {
-#             j_table <- j_table[j_table$Type == input$Type,]
-#         }
-#         if (input$Cost != "All") {
-#           j_table <- j_table[j_table$Cost == input$Cost,]
-#         }
-#         
-#         
-#         
-#         j_table
-#     }, escape = FALSE, rownames = FALSE))
+    output$ex2 <- DT::renderDataTable(DT::datatable({
+    data2
+    }, escape = FALSE, rownames = FALSE))
 } 
 
-
-
-######################### RUN ###########################################################
+######################### RUN
 
 shinyApp(ui, server)
 
--- ownership_analysis.R
+++ ownership_analysis.R
@@ -7,8 +7,13 @@ ownership <- observations %>%
   select(Owner) %>% 
   group_by(Owner) %>% 
   mutate(tot_owner = n(),
-         perc_owner = round(tot_owner / nrow(observations) * 100, 2)) %>% 
-  distinct()
+         perc_owner = round(tot_owner / nrow(observations) * 100, 1)) %>% 
+  distinct() %>% 
+  arrange(tot_owner) %>% 
+  ungroup() %>% 
+  mutate(cumsum = cumsum(perc_owner))
+
+write_csv(ownership, "ownership.csv")
 
 ownership_lad <- observations %>% 
   select(LAD, Owner) %>% 
@@ -19,10 +24,19 @@ ownership_lad <- observations %>%
          perc_owner_lad = round(tot_owner_lad / tot_LAD * 100, 0)) %>% 
   distinct()
 
+
+monopolies <- ownership_lad %>% 
+  filter(perc_owner_lad == 100) 
+
+write_csv(monopolies, "monopolies.csv")
+
+
 histogram <- ownership_lad %>% 
   left_join(counts_for_lads, by = c("LAD")) %>% 
   select(LAD, Owner.x, perc_owner_lad, tot_owner_lad, `Total number of titles in the same Local Authority District`) %>% 
   distinct() %>% 
-  pivot_wider(id_cols = c(LAD, tot_owner_lad, `Total number of titles in the same Local Authority District`), names_from = Owner.x, values_from = perc_owner_lad,)
-
+  pivot_wider(id_cols = c(LAD, tot_owner_lad, `Total number of titles in the same Local Authority District`), 
+              names_from = Owner.x, values_from = perc_owner_lad,) %>%
+  mutate(across(where(is.numeric), function(x) tidyr::replace_na(x, 0))) 
+  
 write_csv(histogram, "ownership_shares.csv")
